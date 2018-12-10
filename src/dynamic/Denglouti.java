77b57a1d853b2e2504d180016702e511571dab5b
hola <holakt@qq.com>
2018-12-10 11:59:01 +0800

    93以后long会溢出，可以使用BigInteger

--- src/dynamic/Denglouti.java
+++ src/dynamic/Denglouti.java
@@ -1,5 +1,6 @@
 package dynamic;
 
+import java.math.BigInteger;
 import java.util.Scanner;
 
 /**
@@ -11,7 +12,7 @@ import java.util.Scanner;
  * 小明上楼有一个习惯，就是一定会先迈出左脚上楼梯，今天小明突发奇想，我能不能最后一步也是迈左脚呢？他数了一下楼梯，一层楼恰好9梯，于是，他左脚1梯、右脚1梯、左1、右1、左1、右1、左1、右1、左1，最后一次恰好迈出的是左脚，但他马上又觉得9梯为奇数是个巧合，于是找了一个仅有4梯的平台，发现只要允许一步跨2梯，仍然可以最后一次迈左脚登上平台，而且方案还不是一种，可以是“左1，右2，左1”，“左2，右1，左1”，“左1，右1，左2”等不同方法，于是小明就很想知道，按自己的习惯和要求（允许一步可以跨1~2梯），登上9梯平台应该有多少种登梯方案？那么若有10梯、20梯的平台又应该有多少种登梯方案呢？
  * <p>
  * 输入
- * 每次输一个数字N：0 ≤ N < 97，代表一个平台有多少梯。最后一次，输入0结束。
+ * 每次输一个数字N：0 ≤ N < 93，代表一个平台有多少梯。最后一次，输入0结束。
  * 输出
  * 若输入数据大于1，请输出对应的登梯方案数量，并以一个换行符结束。
  * 样例输入
@@ -29,9 +30,9 @@ import java.util.Scanner;
 public class Denglouti {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int b[] = new int[97];
+        int b[] = new int[93];
         int max = 0, f;
-        for (int i = 0; i < 97; i++) {
+        for (int i = 0; i < 93; i++) {
             f = sc.nextInt();
             if (f == 0) {
                 break;
