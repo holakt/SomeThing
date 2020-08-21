5284ac0cf0f929419493c4f2023d3364f85b2129
holate <holate@163.com>
2020-08-21 15:02:49 +0800

    [leetcode]:solution111 comment

--- src/leetcodecn/Solution111.java
+++ src/leetcodecn/Solution111.java
@@ -26,44 +26,44 @@ import java.util.List;
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class Solution111 {
-
-    public int minDepth(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-        List<TreeNode> list = new LinkedList<>();
-        List<Integer> deep = new ArrayList<>();
-        list.add(root);
-        deep.add(1);
-        TreeNode next;
-        while (!list.isEmpty()) {
-            next = list.get(0);
-            int d = deep.get(0);
-            if (next == null) {
-                list.remove(0);
-                deep.remove(0);
-                continue;
-            }
-            if (next.right == null && next.left == null) {
-                return d;
-            }
-            list.add(next.left);
-            deep.add(d + 1);
-            list.add(next.right);
-            deep.add(d + 1);
-            list.remove(0);
-            deep.remove(0);
-        }
-        return 0;
-    }
+//
+//    public int minDepth(TreeNode root) {
+//        if (root == null) {
+//            return 0;
+//        }
+//        List<TreeNode> list = new LinkedList<>();
+//        List<Integer> deep = new ArrayList<>();
+//        list.add(root);
+//        deep.add(1);
+//        TreeNode next;
+//        while (!list.isEmpty()) {
+//            next = list.get(0);
+//            int d = deep.get(0);
+//            if (next == null) {
+//                list.remove(0);
+//                deep.remove(0);
+//                continue;
+//            }
+//            if (next.right == null && next.left == null) {
+//                return d;
+//            }
+//            list.add(next.left);
+//            deep.add(d + 1);
+//            list.add(next.right);
+//            deep.add(d + 1);
+//            list.remove(0);
+//            deep.remove(0);
+//        }
+//        return 0;
+//    }
 }
-
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode(int x) {
-        val = x;
-    }
-}
+//
+//class TreeNode {
+//    int val;
+//    TreeNode left;
+//    TreeNode right;
+//
+//    TreeNode(int x) {
+//        val = x;
+//    }
+//}
