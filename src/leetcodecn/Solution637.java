595661d77763d7f7b9087175c5ecfca0115dd8b5
holate <holate@163.com>
2020-09-12 11:39:56 +0800

    [leetcode]:Solution637 resolve

---
+++ src/leetcodecn/Solution637.java
@@ -0,0 +1,61 @@
+package leetcodecn;
+
+/*
+给定一个非空二叉树, 返回一个由每层节点平均值组成的数组。
+
+示例 1：
+
+输入：
+    3
+   / \
+  9  20
+    /  \
+   15   7
+输出：[3, 14.5, 11]
+解释：
+第 0 层的平均值是 3 ,  第1层是 14.5 , 第2层是 11 。因此返回 [3, 14.5, 11] 。
+
+提示：
+
+节点值的范围在32位有符号整数范围内。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/average-of-levels-in-binary-tree
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution637 {
+    /*
+    public List<Double> averageOfLevels(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<Double> res = new ArrayList<>();
+        if (root == null) return new ArrayList<>();
+        queue.offer(root);
+        while (queue.size() > 0) {
+            int size = queue.size();
+            int i = size;
+            Long sum = null;
+            while (size-- > 0) {
+                TreeNode treeNode = queue.poll();
+                sum = sum == null ? 0 : sum;
+                sum += treeNode.val;
+                if (treeNode.left != null)
+                    queue.add(treeNode.left);
+                if (treeNode.right != null)
+                    queue.add(treeNode.right);
+            }
+            res.add(sum / (double) i);
+        }
+        return res;
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+    */
+}
