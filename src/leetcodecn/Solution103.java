777fcac1d18fed72f2b0c5b1b362b22028839bd6
holate <holate@163.com>
2020-12-23 17:16:51 +0800

    [leetcode]:Solution103 resolve

--- src/leetcodecn/Solution103.java
+++ src/leetcodecn/Solution103.java
@@ -6,6 +6,30 @@ import java.util.List;
 import java.util.Queue;
 
 /**
+ * 给定一个二叉树，返回其节点值的锯齿形层序遍历。（即先从左往右，再从右往左进行下一层遍历，以此类推，层与层之间交替进行）。
+ * <p>
+ * 例如：
+ * 给定二叉树[3,9,20,null,null,15,7],
+ * <p>
+ * 3
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
+ * 返回锯齿形层序遍历如下：
+ * <p>
+ * [
+ * [3],
+ * [20,9],
+ * [15,7]
+ * ]
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/binary-tree-zigzag-level-order-traversal
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * 执行用时：1 ms, 在所有 Java 提交中击败了98.12%的用户
+ * 内存消耗：38.8 MB, 在所有 Java 提交中击败了19.94%的用户
+ *
  * @author holate
  * @date 2020/12/23
  */
@@ -19,39 +43,42 @@ public class Solution103 {
      * TreeNode(int x) { val = x; }
      * }
      */
-
+//    public static void main(String[] args) {
+//        TreeNode root = new TreeNode(3);
+//        TreeNode r1 = new TreeNode(9);
+//        TreeNode r2 = new TreeNode(20);
+//        TreeNode r21 = new TreeNode(15);
+//        TreeNode r22 = new TreeNode(7);
+//        root.left = r1;
+//        root.right = r2;
+//        r2.left = r21;
+//        r2.right = r22;
+//        new Solution103().zigzagLevelOrder(root);
+//    }
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        Queue<Pair> queue = new LinkedList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
         ArrayList<List<Integer>> lists = new ArrayList<>(new ArrayList<>());
         if (root == null) {
             return lists;
         }
-        queue.offer(new Pair(root, 0));
-        while (queue.size() != 0) {
-            Pair now = queue.poll();
-            if (lists.size() < now.deep + 1) {
-                lists.add(new ArrayList<>());
+        queue.offer(root);
+        int deep = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode now = queue.poll();
+                if (deep % 2 == 0) list.add(now.val);
+                else list.add(0, now.val);
+                if (now.left != null) queue.add(now.left);
+                if (now.right != null) queue.add(now.right);
             }
-            if (now.deep % 2 == 0) {
-                lists.get(now.deep).add(now.node.val);
-            } else {
-                lists.get(now.deep).add(0, now.node.val);
-            }
-            if (now.node.left != null) queue.offer(new Pair(now.node.left, now.deep + 1));
-            if (now.node.right != null) queue.offer(new Pair(now.node.right, now.deep + 1));
+            deep++;
+            lists.add(list);
         }
         return lists;
     }
 
-    static class Pair {
-        TreeNode node;
-        int deep;
-
-        public Pair(TreeNode node, int deep) {
-            this.node = node;
-            this.deep = deep;
-        }
-    }
 }
 
 class TreeNode {
