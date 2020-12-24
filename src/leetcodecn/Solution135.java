71c9aea287ee87f702ef04d514702a50fc86dc76
holate <holate@163.com>
2020-12-24 15:54:16 +0800

    [leetcode]:Solution135 resolve

---
+++ src/leetcodecn/Solution135.java
@@ -0,0 +1,56 @@
+package leetcodecn;
+
+/**
+ * 老师想给孩子们分发糖果，有 N个孩子站成了一条直线，老师会根据每个孩子的表现，预先给他们评分。
+ * <p>
+ * 你需要按照以下要求，帮助老师给这些孩子分发糖果：
+ * <p>
+ * 每个孩子至少分配到 1 个糖果。
+ * 相邻的孩子中，评分高的孩子必须获得更多的糖果。
+ * 那么这样下来，老师至少需要准备多少颗糖果呢？
+ * <p>
+ * 示例1:
+ * <p>
+ * 输入: [1,0,2]
+ * 输出: 5
+ * 解释: 你可以分别给这三个孩子分发 2、1、2 颗糖果。
+ * 示例2:
+ * <p>
+ * 输入: [1,2,2]
+ * 输出: 4
+ * 解释: 你可以分别给这三个孩子分发 1、2、1 颗糖果。
+ * 第三个孩子只得到 1 颗糖果，这已满足上述两个条件。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/candy
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author holate
+ * @date 2020/12/24
+ */
+public class Solution135 {
+    public int candy(int[] ratings) {
+        if (ratings.length == 0) {
+            return 0;
+        }
+        int ret = 1;
+        int len = ratings.length;
+        int pre = 1, inc = 0, dec = 0;
+        for (int i = 1; i < len; i++) {
+            if (ratings[i] >= ratings[i - 1]) {
+                dec = 0;
+                pre = ratings[i] == ratings[i - 1] ? 1 : pre + 1;
+                ret += pre;
+                inc = pre;
+            } else {
+                dec++;
+                if (dec == inc) {
+                    dec++;
+                }
+                ret += dec;
+                pre = 1;
+            }
+        }
+        return ret;
+    }
+}
