0e5c3234dc4a5a9ec84519fe306305793afd6925
holate <holate@163.com>
2021-05-15 12:08:52 +0800

    [leetcode][easy]:solution13 resolve2(charAt better than toCharArray)

--- src/leetcodecn/Solution13.java
+++ src/leetcodecn/Solution13.java
@@ -1,49 +1,49 @@
 package leetcodecn;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * @author holate
  * @date 2021/5/15
  */
 public class Solution13 {
-    static Map<String, Integer> map = new HashMap<String, Integer>() {{
-        put("M", 1000);
-        put("CM", 900);
-        put("D", 500);
-        put("CD", 400);
-        put("C", 100);
-        put("XC", 90);
-        put("L", 50);
-        put("XL", 40);
-        put("X", 10);
-        put("IX", 9);
-        put("V", 5);
-        put("IV", 4);
-        put("I", 1);
-    }};
-
     public static void main(String[] args) {
         int num = new Solution13().romanToInt("MCMXCIV");
         System.out.println(num);
     }
 
-    public int romanToInt(String s) {
-        char[] array = s.toCharArray();
-        int num = 0;
-        for (int i = 0; i < array.length; i++) {
-            String a1 = String.valueOf(array[i]);
-            if (i + 1 < array.length) {
-                String a2 = a1 + array[i + 1];
-                if (map.containsKey(a2)) {
-                    num += map.get(a2);
-                    ++i;
-                    continue;
-                }
-            }
-            num += map.get(a1);
+    private int getValue(char ch) {
+        switch (ch) {
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+            default:
+                return 0;
         }
-        return num;
+    }
+
+    public int romanToInt(String s) {
+        int ans = 0;
+        int pre = getValue(s.charAt(0));
+        for (int i = 1; i < s.length(); i++) {
+            int num = getValue(s.charAt(i));
+            if (pre < num) {
+                ans = ans - pre;
+            } else {
+                ans = ans + pre;
+            }
+            pre = num;
+        }
+        ans = ans + pre;
+        return ans;
     }
 }
