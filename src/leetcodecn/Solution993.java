b3aa56db1853338690b01b52aa7bd503f4bbf15b
holate <holate@163.com>
2021-05-17 14:50:34 +0800

    [leetcode][easy]:Solution993 递归 resolve [0ms 100%]

--- src/leetcodecn/Solution993.java
+++ src/leetcodecn/Solution993.java
@@ -5,42 +5,36 @@ package leetcodecn;
  * @date 2021/5/17
  */
 public class Solution993 {
-/*    public static void main(String[] args) {
-        System.out.println(1 ^ 3);
-        System.out.println(2 ^ 3);
+    public int deepNode(TreeNode root, int n, int deep) {
+        if (root != null) {
+            if (root.val == n) {
+                return deep;
+            }
+            int l = deepNode(root.left, n, deep + 1);
+            int r = deepNode(root.right, n, deep + 1);
+            return l == -1 ? r : l;
+        }
+        return -1;
+    }
+
+    public boolean fatherNodeVal(TreeNode root, int x, int y) {
+        if (root != null) {
+            if (root.left != null && root.right != null) {
+                if ((root.left.val == x && root.right.val == y) || (root.left.val == y && root.right.val == x)) {
+                    return false;
+                }
+            }
+            return fatherNodeVal(root.left, x, y) && fatherNodeVal(root.right, x, y);
+        }
+        return true;
     }
 
     public boolean isCousins(TreeNode root, int x, int y) {
-        Queue<TreeNode> nodeList = new LinkedList<>();
-        nodeList.add(root);
-        while (!nodeList.isEmpty()) {
-            int size = nodeList.size();
-            int t = 0;
-            for (int j = 0; j < size; j++) {
-                TreeNode curNode = nodeList.poll();
-                if (curNode == null) {
-                    continue;
-                }
-                if (curNode.left != null && curNode.right != null) {
-                    if ((x == curNode.left.val && y == curNode.right.val) ||
-                        (y == curNode.left.val && x == curNode.right.val)) {
-                        return false;
-                    }
-                }
-                if (t != 0 && t == curNode.val) {
-                    return true;
-                } else if (curNode.val == x || curNode.val == y) {
-                    t = curNode.val ^ x ^ y;
-                }
-                nodeList.add(curNode.left);
-                nodeList.add(curNode.right);
-            }
-        }
-        return false;
-    }*/
+        return deepNode(root, x, 0) == deepNode(root, y, 0) && fatherNodeVal(root, x, y);
+    }
 }
 
-/*class TreeNode {
+class TreeNode {
     int val;
     TreeNode left;
     TreeNode right;
@@ -57,4 +51,4 @@ public class Solution993 {
         this.left = left;
         this.right = right;
     }
-}*/
+}
