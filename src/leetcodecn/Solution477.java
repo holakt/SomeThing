a095542a67695e56fcc0862346c970502ab8119d
holate <holate@163.com>
2021-05-28 14:43:25 +0800

    [leetcode][medium]:Solution477 resolve [5ms 100%]

---
+++ src/leetcodecn/Solution477.java
@@ -0,0 +1,19 @@
+package leetcodecn;
+
+/**
+ * @author holate
+ * @date 2021/5/28
+ */
+public class Solution477 {
+    public int totalHammingDistance(int[] nums) {
+        int ans = 0, n = nums.length;
+        for (int i = 0; i < 30; ++i) {
+            int c = 0;
+            for (int val : nums) {
+                c += (val >>> i) & 1;
+            }
+            ans += c * (n - c);
+        }
+        return ans;
+    }
+}
